161dd7b575be80ca9424bcc4c862674352fb29f4
Jennifer <jennifer@MacBook-Jen-Carrillo.local>
2022-09-30 11:18:42 -0500

    Adds missing tests to controller

--- src/test/java/com/example/bank_test/controller/ClienteControllerTest.java
+++ src/test/java/com/example/bank_test/controller/ClienteControllerTest.java
@@ -1,7 +1,47 @@
 package com.example.bank_test.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.bank_test.model.dto.ClienteRequestDTO;
+import com.example.bank_test.service.ClienteService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
 class ClienteControllerTest {
+    @Mock
+    private ClienteService clienteService;
+    @InjectMocks
+    ClienteController clienteController;
 
+    @Test
+    public void shouldReturnSuccessfulResponseWhenRequestIsOk()
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String nombre = "Jose Perez";
+        String identificacion = "202930";
+        String direccion = "Quito";
+        String telefono = "0994923933";
+        String genero = "masculino";
+        int edad = 20;
+        String clienteId = "jose123";
+        String contrasena = "passjose123";
+        ClienteRequestDTO clienteRequestDTO = new ClienteRequestDTO(nombre, identificacion,
+                direccion, telefono, genero, edad, clienteId, contrasena);
+
+        ResponseEntity<String> responseEntity = clienteController.createCliente(clienteRequestDTO);
+
+        verify(clienteService, times(1)).saveClienteInformation(clienteRequestDTO);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getBody()).isEqualTo("Cliente creado exitosamente");
+    }
 }
--- src/test/java/com/example/bank_test/controller/MovimientoControllerTest.java
+++ src/test/java/com/example/bank_test/controller/MovimientoControllerTest.java
@@ -1,7 +1,40 @@
 package com.example.bank_test.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.bank_test.model.dto.MovimientoRequestDTO;
+import com.example.bank_test.service.MovimientoService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
 class MovimientoControllerTest {
+    @Mock
+    private MovimientoService movimientoService;
+    @InjectMocks
+    MovimientoController movimientoController;
+    @Test
+    public void shouldReturnSuccessfulResponseWhenMovimientoIsOk()
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String tipoMovimiento = "debito";
+        double valor = 15.0;
+        String numeroCuenta = "2345";
+        MovimientoRequestDTO movimientoRequestDTO = new MovimientoRequestDTO(tipoMovimiento, valor, numeroCuenta);
 
+        ResponseEntity<String> responseEntity = movimientoController.createMovimiento(movimientoRequestDTO);
+
+        verify(movimientoService, times(1)).saveMovimiento(movimientoRequestDTO);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getBody()).isEqualTo("Transaccion realizada exitosamente");
+    }
 }
